fb2ccf6765de50a98f9d514bf719452782f4d99b
S. Pratham <educate.pratham@gmail.com>
2026-09-15 16:54:28 +0530

    removed the /backend

--- backend/filters/geometric_compostion/canvas.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-from typing import Tuple
-
-def create_canvas(width: int = 1080, height: int = 1080, background_color: Tuple[int, int, int] = (15, 15, 15)):
-    canvas = Image.new('RGB', (width, height), background_color)
-    return canvas
-
-if __name__ == __main___:
-    
